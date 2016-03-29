5ecc689d62e71791ef57f37e576d38ba079575a4
Ian Rose <ianrosedev@gmail.com>
2016-03-29 19:29:08 -0400

    [wip] Have a bit more JS working, printing to page

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-$(function() {
+//$(function() {
   var twitchUsers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "cretetion"];
+  
+  var myString = "";
   
   for (var i = 0; i < twitchUsers.length; i++) {
     $.getJSON('https://api.twitch.tv/kraken/streams/' + twitchUsers[i] + '?callback=?',  function(data) {
-      var returnHTML = '';
       if (data.stream === null) {
-        returnHTML += '<p>' + twitchUsers[i] + ' offline</p>';
+        myString += '<h1>' + data.stream +'</h1><br>';
         console.log(data);
         console.log('not online');
+        $('#main-text').html(myString);
       } else {
-        returnHTML += '<p>' + twitchUsers[i] + ' ' + data.stream.game + '</p>';
+        myString += '<h1>' + data.stream.game +'</h1><br>';
         console.log(data);
         console.log(data.stream.game);
+        $('#main-text').html(myString);
       }
-     
     });
+    console.log(twitchUsers[i]);
   }
-  
-   $('#main-text').html(returnHTML);
-});
+//});
