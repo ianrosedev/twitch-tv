9fc23d3bc3d801cea325d4b5f2597bd5bc9c38cd
Ian Rose <ianrosedev@gmail.com>
2016-03-29 22:28:59 -0400

    Made smiley-face image default if no given img

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ $(function() {
   for (var i = 0; i < twitchUsers.length; i++) {
     $.getJSON('https://api.twitch.tv/kraken/channels/' + twitchUsers[i] + '?callback=?', function(data) {
       if (data) {
-        myString += '<p class="display-data"><a href="' +  data.url + '">' + '<img src="' + data.logo + '" class=\"thumbnail\">' + data.display_name + '</a></p>';
+        var thumbnailIMG = data.logo;
+        if (thumbnailIMG === null) {
+          thumbnailIMG = 'smiley.jpg';
+        }
+        
+        myString += '<p class="display-data"><a href="' +  data.url + '">' + '<img src="' + thumbnailIMG + '" class=\"thumbnail\">' + data.display_name + '</a></p>';
         $('#main-text').html(myString);
         console.log(data);
       } else {
