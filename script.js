7fa202e9f41bbd5415a3ab76a0a8320085b51355
Ian Rose <ianrosedev@gmail.com>
2016-03-29 21:28:17 -0400

    Got URL working

--- script.js
+++ script.js
@@ -4,27 +4,19 @@
 var myString = "";
   
   for (var i = 0; i < twitchUsers.length; i++) {
-    $.getJSON('https://api.twitch.tv/kraken/streams/' + twitchUsers[i] + '?callback=?',  function(data) {
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + twitchUsers[i] + '?callback=?', function(data) {
       if (data) {
-        
+
         $.getJSON(data._links.channel + '?callback=?', function(data) {
-          myString += '<p>' + data.display_name + '</p><br>';
+          myString += '<p><a href="' + data.url + '">' + data.display_name + '</a></p><br>';
           $('#main-text').html(myString);
           console.log(data);
         });
         
-        
-        
-        
-        //myString += '<p>' + data._links.channel +'</p><br>';
-        console.log('not online')
         $('#main-text').html(myString);
+        
       } else {
-        //myString += '<p>' + data.stream.game +'</p>';
-        myString += '<p>' + data._links.channel +'</p><br>';
-        console.log(data);
-        //console.log(data.stream.game);
-        $('#main-text').html(myString);
+        console.log('ERR');
       }
     });
   }
