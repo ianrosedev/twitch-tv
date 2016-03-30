6222797c47f88884d0d1579cab655c97a5916dac
Ian Rose <ianrosedev@gmail.com>
2016-03-29 21:35:55 -0400

    Have API working

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-//$(function() {
+$(function() {
   var twitchUsers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "cretetion"];
   
 var myString = "";
   
   for (var i = 0; i < twitchUsers.length; i++) {
-    $.getJSON('https://api.twitch.tv/kraken/streams/' + twitchUsers[i] + '?callback=?', function(data) {
+    $.getJSON('https://api.twitch.tv/kraken/channels/' + twitchUsers[i] + '?callback=?', function(data) {
       if (data) {
-
-        $.getJSON(data._links.channel + '?callback=?', function(data) {
-          myString += '<p><a href="' + data.url + '">' + data.display_name + '</a></p><br>';
-          $('#main-text').html(myString);
-          console.log(data);
-        });
-        
+        myString += '<p><a href="' + data.url + '">' + data.display_name + '</a></p><br>';
         $('#main-text').html(myString);
-        
+        console.log(data);
       } else {
         console.log('ERR');
       }
     });
   }
-//});
+});
