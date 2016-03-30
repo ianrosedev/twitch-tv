6c81a281c389b6304f60e05f7a778e5da0a49f93
Ian Rose <ianrosedev@gmail.com>
2016-03-29 21:11:27 -0400

    Working through JSON

--- script.js
+++ script.js
@@ -5,16 +5,25 @@ var myString = "";
   
   for (var i = 0; i < twitchUsers.length; i++) {
     $.getJSON('https://api.twitch.tv/kraken/streams/' + twitchUsers[i] + '?callback=?',  function(data) {
-      if (data.stream === null) {
-        myString += '<p>' + data._links.channel +'</p><br>';
-        console.log(data);
+      if (data) {
+        
+        $.getJSON(data._links.channel + '?callback=?', function(data) {
+          myString += '<p>' + data.display_name + '</p><br>';
+          $('#main-text').html(myString);
+          console.log(data);
+        });
+        
+        
+        
+        
+        //myString += '<p>' + data._links.channel +'</p><br>';
         console.log('not online')
         $('#main-text').html(myString);
       } else {
-        myString += '<p>' + data.stream.game +'</p>';
+        //myString += '<p>' + data.stream.game +'</p>';
         myString += '<p>' + data._links.channel +'</p><br>';
         console.log(data);
-        console.log(data.stream.game);
+        //console.log(data.stream.game);
         $('#main-text').html(myString);
       }
     });
