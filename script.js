cc0b7cef79847c713296d976026fc7782a4b082d
Ian Rose <ianrosedev@gmail.com>
2016-03-29 22:10:37 -0400

    Have thumbnails in

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 $(function() {
   var twitchUsers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "cretetion"];
-  
-var myString = "";
+  var myString = "";
   
   for (var i = 0; i < twitchUsers.length; i++) {
     $.getJSON('https://api.twitch.tv/kraken/channels/' + twitchUsers[i] + '?callback=?', function(data) {
       if (data) {
-        myString += '<p><a href="' + data.url + '">' + data.display_name + '</a></p><br>';
+        myString += '<p class="display-data"><a href="' +  data.url + '">' + '<img src="' + data.logo + '" class=\"thumbnail\">' + data.display_name + '</a></p>';
         $('#main-text').html(myString);
         console.log(data);
       } else {
